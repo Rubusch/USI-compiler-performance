8d8205e0802d1173b071be1ad09642dc62204e93
Rubusch <l.rubusch@gmx.ch>
2013-09-25 21:17:21 +0200

    A03 started with starting code

---
+++ A03__JavaClassDisassember/src/ch/usi/inf/sp/cfg/ControlFlowGraphExtractor.java
@@ -0,0 +1,19 @@
+package ch.usi.inf.sp.cfg;
+
+
+/**
+ * This class extracts a control flow graph in .dot format
+ * from the byte code of a Java method.
+ * 
+ * @author ???
+ */
+public final class ControlFlowGraphExtractor {
+
+	public static void main(final String[] args) {
+		final String classFileName = args[0];
+		final String methodNameAndDescriptor = args[1];
+		
+		// TODO implement this class
+	}
+
+}
