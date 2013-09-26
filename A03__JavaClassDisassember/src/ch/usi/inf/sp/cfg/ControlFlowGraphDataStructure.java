3a9c015f434eeaa4967f5c2d423977e04813743b
Lothar Rubusch <l.rubusch@gmx.ch>
2013-09-26 10:19:12 +0200

    A03, get jump target addr

--- A03__JavaClassDisassember/src/ch/usi/inf/sp/cfg/ControlFlowGraphDataStructure.java
+++ A03__JavaClassDisassember/src/ch/usi/inf/sp/cfg/ControlFlowGraphDataStructure.java
@@ -5,6 +5,8 @@ import java.util.HashMap;
 
 import org.objectweb.asm.tree.AbstractInsnNode;
 import org.objectweb.asm.tree.InsnList;
+import org.objectweb.asm.tree.JumpInsnNode;
+import org.objectweb.asm.tree.LabelNode;
 import org.objectweb.asm.util.Printer;
 
 // TODO perhaps better inherit from ArrayList< ArrayList >
@@ -65,6 +67,13 @@ public class ControlFlowGraphDataStructure {
 // TODO
 	}
 
+	private void forward(){
+		// TODO
+	}
+	
+	private void backward(){
+		// TODO
+	}
 
 	public void appendInstruction( final AbstractInsnNode ins, final int idx, final InsnList instructions ){
 		final int opcode = ins.getOpcode();
@@ -73,13 +82,14 @@ public class ControlFlowGraphDataStructure {
 		// start new list, when either in srctable.get(ins.getOpcode()) is not null, or for 'if' instrs
 		// if
 		// TODO
+//		System.out.println("XXX ins " + ins.getType() + ", mnemonic " + mnemonic);
 
 		// append to basicblocklist or start new basic block, when 
 		if( AbstractInsnNode.JUMP_INSN == ins.getType() ){
 			// extract dest addr, idx will be source
-			int dest = 0;
-// TODO
 
+			final LabelNode targetInstruction = ((JumpInsnNode)ins).label;
+			final int dest = instructions.indexOf(targetInstruction);
 			if( idx < dest ){
 				// forward
 				srctable.put( String.valueOf(dest), String.valueOf(idx) );
