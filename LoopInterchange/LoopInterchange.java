bd42ea3ddd339a81fae7dca9f4c9a56bd28ea559
Lothar Rubusch <l.rubusch@gmx.ch>
2013-10-02 13:27:09 +0200

    LoopInterchange fixed

--- LoopInterchange/LoopInterchange.java
+++ LoopInterchange/LoopInterchange.java
@@ -1,45 +1,60 @@
 public class LoopInterchange {
-    private final int a = 4000;
-    private final int b = 1000;
+    private final int maxRows = 4000;
+    private final int maxCols = 1000;
 
-    private String[][] arr = new String[a][b];
+    private String[][] arr = new String[maxRows][maxCols];
 
     public LoopInterchange(){
         init();
     }
 
     private void init(){
-        System.out.println("init");
-        int timeA = System.nanotime();
-/*
-        for(int i=0; i<a; i++){
-            for(int j=0; j<b; j++){
+
+    	System.out.println("start");
+        long timeStart = System.nanoTime();
+        // the following makes a difference, since a regular compiler can, but will not optimize this, typically
+//*
+        for(int i=0; i<maxRows; i++){
+            for(int j=0; j<maxCols; j++){
                 arr[i][j] = new String();
                 arr[i][j] = String.valueOf(i*j);
             }
         }
 /*/
-        for(int j=0; j<b; j++){
-            for(int i=0; i<a; i++){
+        for(int j=0; j<maxCols; j++){
+            for(int i=0; i<maxRows; i++){
                 arr[i][j] = new String();
                 arr[i][j] = String.valueOf(i*j);
             }
         }
 //*/
-        int timeB = System.nanotime();
+        long timeEnd = System.nanoTime();
 
-        System.out.println( "diff time: " + (timeB - timeA) );
+        System.out.println("end");
+        long timeDiff = (timeEnd - timeStart)/1000000;
+
+        System.out.println( "diff time: " + timeDiff + " ms\n");
     }
 
     public void getVal(){
         System.out.println("2*2 = " + arr[2][2]);
     }
 
-    /////////////////   main()   /////////////////
+
+
     public static void main(String[] args){
+    	try{
             LoopInterchange a01 = new LoopInterchange();
+            LoopInterchange a02 = new LoopInterchange();
+            LoopInterchange a03 = new LoopInterchange();
+            LoopInterchange a04 = new LoopInterchange();
+            LoopInterchange a05 = new LoopInterchange();
+    	}catch(OutOfMemoryError exp){
+    		System.out.println("FAILED");
+    		exp.printStackTrace();
+    	}
 //            a01.getVal();
 
-            System.out.println("READY.");
+            System.out.println("\nREADY.");
     }
 }
