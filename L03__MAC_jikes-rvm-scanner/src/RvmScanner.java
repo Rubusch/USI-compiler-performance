2459bce49eeabbb98caf1a58bb6de2e934a5ebe0
Rubusch <l.rubusch@gmx.ch>
2013-10-09 18:15:45 +0200

    L03, rvm scanner, runs for MAC OS/X only and jikes RVM

---
+++ L03__MAC_jikes-rvm-scanner/src/RvmScanner.java
@@ -0,0 +1,29 @@
+
+public class RvmScanner {
+/*
+ * see the source of method org.jikesrvm.mm.mmtk.ScanBootImage.processChunk as reference implementation (ramp)
+ * 
+ * extract object's class, use method:
+ * org.jikesrvm.tools.bootImageViewer.ImageDecoder.getObjectClassDescriptor
+ * 
+ * first address of boot image data section obtain from
+ * org.jikesrvm.tools.bootImageViewer.ImageDecoder.getBootImageDataStartAddress
+ * 
+ * first address of ramp from
+ * org.jikesrvm.tools.bootImageViewer.ImageDecoder.getBootImageRmapStartAddress
+ * 
+ * load a byte at a given address form the rvm's memory
+ * org.jikesrvm.tools.bootImageViewer.Image.loadByte
+ * 
+ * load reference to byte use
+ * org.jikesrvm.tools.bootImageViewer.Image.loadAddress
+ */
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
