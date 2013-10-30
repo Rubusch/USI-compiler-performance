271c5b562bac6a032df9ebe4315e45de818a605d
Lothar Rubusch <l.rubusch@gmx.ch>
2013-10-30 23:27:05 +0100

    A08 - preparation

---
+++ A08__CacheSimulator/src/ch/usi/inf/sp/simulator/cache/SetAssociativeCacheSimulator.java
@@ -0,0 +1,71 @@
+package ch.usi.inf.sp.simulator.cache;
+
+public class SetAssociativeCacheSimulator implements
+		ISetAssociativeCacheSimulator {
+
+	public SetAssociativeCacheSimulator(int i, int j, int k) {
+		// TODO Auto-generated constructor stub
+	}
+
+
+	@Override
+	public int getNumberOfBitsForTag() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getNumberOfBitsForSet() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getNumberOfBitsForByteInLine() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getNumberOfSets() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getNumberOfWays() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getNumberOfBytesInLine() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getCacheSizeInBytes() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean handleMemoryAccess(int address) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public long getHitCount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public long getMissCount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
