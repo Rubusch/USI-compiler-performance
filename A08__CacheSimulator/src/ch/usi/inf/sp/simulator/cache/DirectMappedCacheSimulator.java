2421262eb7a6aa9e9a014837838d35db872f73e4
Lothar Rubusch <l.rubusch@gmx.ch>
2013-11-05 18:30:53 +0100

    A08, cleanup Direct Mapped Cache comments

--- A08__CacheSimulator/src/ch/usi/inf/sp/simulator/cache/DirectMappedCacheSimulator.java
+++ A08__CacheSimulator/src/ch/usi/inf/sp/simulator/cache/DirectMappedCacheSimulator.java
@@ -20,7 +20,7 @@ public final class DirectMappedCacheSimulator implements IDirectMappedCacheSimul
 
 	/**
 	 * bitsForLine = 9;       // 2^9 == 512 lines
-	 * bitsForByteInByte = 6; // 2^6 ==  64 byte line size
+	 * bitsForByteInLine = 6; // 2^6 ==  64 byte line size
 	 */
 	public DirectMappedCacheSimulator() {
 		this(9, 6);
@@ -57,26 +57,10 @@ public final class DirectMappedCacheSimulator implements IDirectMappedCacheSimul
 	public void handleMemoryAccess(final int address) {
 		System.out.printf("address: 0x%08x (%d)\n", address, address);
 
-//		Tester.db("\taddress\t>> bitsForByteInLine"); // XXX
-//		Tester.db("\t" + String.valueOf(address) + "\t>> " + String.valueOf(bitsForByteInLine) + "\t\t\t\t\t\t\t" + String.valueOf(address>>bitsForByteInLine)); // XXX
-
-//		Tester.db("\t1<< bitsForLine"); // XXX
-//		Tester.db("\t1<< "+ String.valueOf(bitsForLine) + "\t\t\t\t\t\t\t\t" + String.valueOf(1<<bitsForLine)); // XXX
-
 		final int line = (address>>bitsForByteInLine)&((1<<bitsForLine)-1);
-
-		Tester.db("\t(address\t>> bitsForByteInLine)\t& ((1<<bitsForLine)-1)");
-		Tester.db("\t(" + String.valueOf(address) + "\t>> " + String.valueOf(bitsForByteInLine) + ")\t\t\t& ((" + String.valueOf(1<<bitsForLine) + ")-1)\t\t" + String.valueOf(line)); // XXX
-
 		System.out.printf("line:    0x%08x (%d)\n", line, line);
 
-//		Tester.die(); // XXX
-
 		final int tag = address>>>(bitsForLine+bitsForByteInLine);
-		
-		Tester.db("\taddress\t>>>(bitsForLine + bitsForByteInLine)");
-		Tester.db("\t" + String.valueOf(address) + "\t>>>(" + String.valueOf(bitsForLine) + " + " + String.valueOf(bitsForByteInLine) + ")\t\t\t\t\t\t" + String.valueOf(tag));
-
 		System.out.printf("tag:     0x%08x (%d)\n", tag, tag);
 
 		if (tags[line]==tag && validBits[line]) {
@@ -97,5 +81,4 @@ public final class DirectMappedCacheSimulator implements IDirectMappedCacheSimul
 	public long getMissCount() {
 		return missCount;
 	}
-	
 }
