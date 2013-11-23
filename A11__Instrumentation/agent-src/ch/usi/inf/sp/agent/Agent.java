575b2b346494e08755115a5d056e234c10f5726a
Lothar Rubusch <l.rubusch@gmx.ch>
2013-11-23 23:20:37 +0100

    A11, agent

---
+++ A11__Instrumentation/agent-src/ch/usi/inf/sp/agent/Agent.java
@@ -0,0 +1,14 @@
+package ch.usi.inf.sp.agent;
+
+import java.lang.instrument.Instrumentation;
+
+public class Agent {
+
+	/**
+	 * @param args
+	 */
+	public static void premain(String agentArgs, Instrumentation inst){
+		System.out.println("Agent stating (arguments: '"+agentArgs+"')");
+	}
+
+}
