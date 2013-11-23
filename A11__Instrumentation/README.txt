93fb4e77c2fb4636a46fb08bad9910a93d103af6
Lothar Rubusch <l.rubusch@gmx.ch>
2013-11-23 23:16:14 +0100

    A11, application setup

---
+++ A11__Instrumentation/README.txt
@@ -0,0 +1,8 @@
+README
+
+application
+
+iDEBIAN<23:11:50>::user("/opt/git_java_performance/A11__Instrumentation/");
+$ java -cp application-bin Application
+
+
--- A11__Instrumentation/application-src/Application.java
+++ A11__Instrumentation/application-src/Application.java
@@ -1,11 +1,14 @@
-package ch.usi.inf.sp.application;
+
 
 public class Application {
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Hello");
+		sayHi();
 	}
 
+	private static void sayHi(){
+		System.out.println("Hello");
+	}
 }
